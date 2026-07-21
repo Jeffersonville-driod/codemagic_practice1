bfe3b621300f82bb6d3ebcec337d9abb62432ca7
jefferson <140842465+Jeffersonville-driod@users.noreply.github.com>
2026-07-21 05:28:36 +0100

    Update Kotlin version in build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,12 @@
 buildscript {
-    extra["kotlin_version"] = "1.9.22"
+    val kotlin_version: String by project
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
